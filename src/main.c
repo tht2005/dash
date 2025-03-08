47c8cf6d8b30639e6fe5a6a1f4b4283bf548f443
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-03-09 05:42:25 +0700

    use gnu glibc extension

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
+// int execvpe(const char *file, char *const argv[], char *const envp[]);
+#define _GNU_SOURCE
+#include <unistd.h>
 
-int main() {
+#include "shell.h"
+#include "job.h"
+
+int main()
+{
+    init_shell();
+
     return 0;
 }
