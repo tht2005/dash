7fcff8ea118f05718ee25e2d343a4a47fd7e41e4
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-03-09 05:59:58 +0700

    test execvpe() with PATH environment variable

--- src/main.c
+++ src/main.c
@@ -9,5 +9,14 @@ int main()
 {
     init_shell();
 
+    clearenv();
+    setenv("PATH", "/bin:/usr/bin", 1);
+    printf("PATH=%s\n", getenv("PATH"));
+
+    char *argv[] = { "echo", "Hello, World!", NULL };
+    execvp(argv[0], argv);
+
+    perror("execvp");
+
     return 0;
 }
